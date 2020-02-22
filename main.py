f2202040d41528ed0c5cd73f17c1e5ce4d855039
João Vítor <joao.sferreira@souunit.com.br>
2020-02-22 12:41:10 -0300

    Opções, principais grafos de coloração e extras.py

--- main.py
+++ main.py
@@ -13,23 +13,40 @@ def main():
           +"\nAluno: João Vítor Silva Ferreira"
           +"\nTurma: N01")
 
-    # pegar quantidade de vértices
-    qtdeVertices = int(input("Quantidade de Vértices> "))
+    opcao = int(input("=== Opções ==="
+          +"\n1-Cadastrar Grafo"
+          +"\n2-Principais Grafos de Coloração (Extra)"
+          +"\n: "))
 
-    adjMATRIZ = []
-    for i in range(qtdeVertices):
-        linha = []
-        for j in range(qtdeVertices):
-            adjEntrada = int(input("Adjacência (v" + str(i) + "v" + str(j) + ")(0>não|1>sim):"))
-            if adjEntrada == 0:
-                linha.append(0)
-            elif adjEntrada == 1:
-                linha.append(1)
-            else:
-                print("Apenas é aceito 0 ou 1")
-        adjMATRIZ.append(linha)
+    if opcao == 1:
+        # pegar quantidade de vértices
+        qtdeVertices = int(input("Quantidade de Vértices> "))
 
-    printarAdjMATRIZ(qtdeVertices, adjMATRIZ)
+        if qtdeVertices <= 0:
+            return 0
+
+        adjMATRIZ = []
+        for i in range(qtdeVertices):
+            linha = []
+            for j in range(qtdeVertices):
+                adjEntrada = int(input("Adjacência (v" + str(i) + "v" + str(j) + ")(0>não|1>sim):"))
+                if adjEntrada == 0:
+                    linha.append(0)
+                elif adjEntrada == 1:
+                    linha.append(1)
+                else:
+                    print("Apenas é aceito 0 ou 1!")
+            adjMATRIZ.append(linha)
+
+        print("Grafo cadastrado!")
+        printarAdjMATRIZ(qtdeVertices, adjMATRIZ)
+
+    elif opcao == 2:
+        import extras
+        extras.coloracaoExemplos()
+
+    else:
+        print("Opção não encontrada.")
 
 
 if __name__ == "__main__":
