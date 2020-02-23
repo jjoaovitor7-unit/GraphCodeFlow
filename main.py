d29c3b350657c35a63fefa1026681cee585a47a1
João Vítor <joao.sferreira@souunit.com.br>
2020-02-23 12:44:58 -0300

    +opções

--- main.py
+++ main.py
@@ -27,43 +27,58 @@ def ehCompleto(qtdeV, MATRIZ):
 def main():
     print("=== Atividade de Implementação ==="
           +"\nAluno: João Vítor Silva Ferreira"
-          +"\nTurma: N01")
+          +"\nTurma: N01\n")
 
-    opcao = int(input("=== Opções ==="
-          +"\n1-Cadastrar Grafo"
-          +"\n2-Principais Grafos de Coloração (Extra)"
-          +"\n: "))
+    while True:
+        opcao = int(input("=== Opções ==="
+              +"\n1-Cadastrar Grafo"
+              +"\n2-Principais Grafos de Coloração (Extra)"
+              +"\n3-Printar Grafo"
+              +"\n4-Verificar se o Grafo é completo"
+              +"\n99-Parar o Programa"
+              +"\n: "))
 
-    if opcao == 1:
-        # pegar quantidade de vértices
-        qtdeVertices = int(input("Quantidade de Vértices> "))
+        if opcao == 1:
+            qtdeVertices = int(input("Quantidade de Vértices> "))
 
-        if qtdeVertices <= 0:
+            if qtdeVertices <= 0:
+                return 0
+
+            adjMATRIZ = []
+            for i in range(qtdeVertices):
+                linha = []
+                for j in range(qtdeVertices):
+                    adjEntrada = int(input("Adjacência (v" + str(i) + "v" + str(j) + ")(0>não|1>sim):"))
+                    if adjEntrada == 0:
+                        linha.append(0)
+                    elif adjEntrada == 1:
+                        linha.append(1)
+                    else:
+                        print("Apenas é aceito 0 ou 1!")
+                adjMATRIZ.append(linha)
+            print("Grafo cadastrado!")
+
+        elif opcao == 2:
+            import extras
+            extras.coloracaoExemplos()
+
+        elif opcao == 3:
+            try:
+                printarAdjMATRIZ(qtdeVertices, adjMATRIZ)
+            except UnboundLocalError:
+                print("É necessário cadastrar o Grafo primeiro.")
+
+        elif opcao == 4:
+            try:
+                ehCompleto(qtdeVertices, adjMATRIZ)
+            except UnboundLocalError:
+                print("É necessário cadastrar o Grafo primeiro.")
+
+        elif opcao == 99:
             return 0
 
-        adjMATRIZ = []
-        for i in range(qtdeVertices):
-            linha = []
-            for j in range(qtdeVertices):
-                adjEntrada = int(input("Adjacência (v" + str(i) + "v" + str(j) + ")(0>não|1>sim):"))
-                if adjEntrada == 0:
-                    linha.append(0)
-                elif adjEntrada == 1:
-                    linha.append(1)
-                else:
-                    print("Apenas é aceito 0 ou 1!")
-            adjMATRIZ.append(linha)
-
-        print("Grafo cadastrado!")
-        printarAdjMATRIZ(qtdeVertices, adjMATRIZ)
-        ehCompleto(qtdeVertices, adjMATRIZ)
-
-    elif opcao == 2:
-        import extras
-        extras.coloracaoExemplos()
-
-    else:
-        print("Opção não encontrada.")
+        else:
+            print("Opção não encontrada.")
 
 
 if __name__ == "__main__":
